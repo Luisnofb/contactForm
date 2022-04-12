35bffa942889a05e6ebf7596c38776f1446af312
Luis <lluismf18@gmail.com>
2022-04-12 12:25:24 -0300

    Criação dos projetos React no front e Spring Boot no back

---
+++ backend/src/main/java/com/maximize/contactForm/ContactFormApplication.java
@@ -0,0 +1,13 @@
+package com.maximize.contactForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactFormApplication.class, args);
+	}
+
+}
