dfb60a3064747503e99ce05dff956d06de044993
Luis <luisnofb@outlook.com>
2022-04-13 19:11:40 -0300

    Domain model and database first seed

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user(company, email, name, telephone) VALUES ('Bradesco', 'maria@gmail.com', 'maria', '989457733');
+
+INSERT INTO tb_message (msg, user_id) VALUES ('Oi mundo', 1);
+INSERT INTO tb_message (msg, user_id) VALUES ('Ohana quer dizer familia', 1);
+INSERT INTO tb_message (msg, user_id) VALUES ('Alone in the dark', 1);
