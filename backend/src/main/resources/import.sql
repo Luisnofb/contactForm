cb20a4babc48ab6e19607a3cc98ab900fde2504f
Luis <luisnofb@outlook.com>
2022-04-15 16:15:16 -0300

    First homolog

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,8 @@
-INSERT INTO tb_user(company, email, name, telephone) VALUES ('Bradesco', 'maria@gmail.com', 'maria', '989457733');
+INSERT INTO tb_user(company, email, name, telephone) VALUES ('Bradesco', 'maria@gmail.com', 'Maria', '989457733');
+INSERT INTO tb_user(company, email, name, telephone) VALUES ('Maximize', 'luis@gmail.com', 'Luis', '992179526');
 
 INSERT INTO tb_message (msg, user_email, user_id) VALUES ('Oi mundo', 'maria@gmail.com', 1);
 INSERT INTO tb_message (msg, user_email, user_id) VALUES ('Ohana quer dizer familia', 'maria@gmail.com', 1);
 INSERT INTO tb_message (msg, user_email, user_id) VALUES ('Alone in the dark', 'maria@gmail.com', 1);
+
+INSERT INTO tb_message (msg, user_email, user_id) VALUES ('Amigo estou aqui', 'luis@gmail.com', 2);
